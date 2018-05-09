611184f5fd4f9e3ed1c69431f3ccd6adfc0c5dc2
Marc Bucchieri <wchurch@whitemountainscience.org>
2018-05-09 09:50:04 -0700

    minor changes

---
+++ flask/migrations/versions/f4f9b5594800_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f4f9b5594800
+Revises: 0399ac3705fd
+Create Date: 2018-05-09 09:47:23.131247
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f4f9b5594800'
+down_revision = '0399ac3705fd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
