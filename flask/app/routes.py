fa15c31d10e180059735fb6db689eb3abf774632
Marc Bucchieri <wchurch@whitemountainscience.org>
2018-09-03 07:24:17 -0700

    added csv features

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -11,6 +11,7 @@ import logging
 import json
 import time
 import sys
+import dateutil.parser
 from logging.handlers import RotatingFileHandler
 from include.utils import *
 from include.config import *
@@ -18,6 +19,7 @@ from include.school_subdomains import *
 
 DEFAULT_SUBDOMAIN = "www"
 
+####################################### BASIC ROUTES #######################################
 @app.route("/")
 @app.route("/index")
 def index():
@@ -48,6 +50,223 @@ def framegal():
         frame.height='500'
 
     return render_template('iframe-gal.html', frames=frames)
+
+
+#######################################  USER ROUTES #######################################
+# These routes correspond to pages for registering new users, logging in, and logging out.
+# User accounts are supported by the flask_login library
+#
+# To Do:
+#   Add a route for deleting an account
+#   Create a more secure method for adding user permissions
+#   Add email confirmation for new users
+
+# Handle user login. If login is sucessful, forward them to the page they
+# were trying to access. Otherwise keep them on the login page.
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+# Use the built in flask_login fuction to logout the current user
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# Add a new user account to the database
+# If the user signs up with a @whitemountainscience.org email address
+# set user.wmsi_user to True so they have full permissions on the site
+# For now all new users are given access to the data_story page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        if "whitemountainscience.org" in user.email:
+            user.wmsi_user = True
+            # only for testing, this should be more secure
+        user.data_story = True
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+ 
+
+####################################### SCHOOL SUBDOMAIN ROUTES #######################################
+
+# for now always redirect to the gallery index for school subdomains
+# (note: this must route /gallery/ with a _specific trailing
+# slash_ to the index, because the various files in 
+# gallery will redirect to gallery/ with the slash, and
+# flask will return a 404 if we don't add that slash here
+# (see here: http://flask.pocoo.org/docs/0.12/quickstart/#variable-rules))
+@app.route("/", subdomain="<site_subdomain>")
+def school_index(site_subdomain):
+    return redirect("/gallery/") # direct redirect below
+
+# make sure the root page displays the index
+@app.route("/gallery/", subdomain="<site_subdomain>")
+def subdomain_index(site_subdomain):
+    return school_subdomain(site_subdomain, "index.html")
+
+# any requests to gallery items for each subdomain are passed
+# onto the correct school domain subfolder
+# (thumbs up stores files and SETS LINKS assuming that 
+#  files are stored under /gallery/, so we serve files 
+#  statically (from the filesystem) from there)
+@app.route("/gallery/<path:filename>", subdomain="<site_subdomain>")
+def school_subdomain(site_subdomain, filename):
+    if site_subdomain == DEFAULT_SUBDOMAIN: # www
+        return redirect(url_for('index'))
+    else:
+        return serve_school_subdomain(site_subdomain, filename)
+
+
+
+####################################### DATA STORIES #######################################
+# This section contains routes associated with the Digital Data Stories project./
+# POSTs can be received from a ScratchX project or by using a cURL command:
+# curl -d "project_id=0&data_type=tempC&value=25" -X POST https://wmsinh.org/scratchx
+# The /data-story route and data_story.html page are respondisble for rendering data
+
+
+# Render the data_story.html page. In order to view the page, a user
+# must have the data_story attribute set to true for their account. 
+# While still in development this attribute is set to True for all
+# new users
+@app.route('/data-story')
+@login_required
+def data_story():
+    if current_user.data_story != True:
+        flash("""Sorry, only users with project permissions can see this page. 
+                If you believe your account should be activated with permissions 
+                contact a WMSI administrator to make the change""")
+        return redirect(url_for('index'))
+    datastory = DataStory.query.all()
+    return render_template('data_story.html', title='Digital Data Stories', datastory=datastory, bgcolor='black')
+
+@app.route('/load-csv',methods=['POST','GET'])
+@login_required
+def load_csv():
+    if request.method == 'POST':
+        if request.is_json:
+            content = request.get_json()
+            for item in content:
+                project_id = int(item['project_id'])
+                data_type = str(item['data_type'])
+                sensor_id = int(item['sensor_id'])
+                timestamp = dateutil.parser.parse(item['timestamp'])
+                value = float(item['value'])
+                data = DataStory(timestamp=timestamp, project_id=project_id, data_type=data_type, sensor_id=sensor_id, value=value)
+                db.session.add(data)
+                print(data)
+            
+            db.session.commit()
+            # print(request.get_json())
+            return 'got some data!'
+        else:
+            return 'something went wrong :('
+    if request.method == 'GET':
+        return render_template('load_csv.html', title='Load CSV File', bgcolor='black')
+
+
+
+# This route is pinged on a timer by the data_story page to get database updates
+# The code below depends on both the web page and the server being on the same
+# timer. In the past this has run into issues when the webpage and server clocks
+# are set to different timezones
+@app.route('/get_new_data')
+def get_new_data():
+    since = request.args.get('since', 0.0, type=float)
+    all_data = DataStory.query.all()
+    new_data = []
+    time_float = None
+    # app.logger.warning('checking for new data')
+    for d in all_data:
+        if (sys.version_info > (3, 0)):
+            time_float = d.timestamp.timestamp()
+        else:
+            epoch = datetime.datetime.utcfromtimestamp(0)
+            time_float =  (d.timestamp - epoch).total_seconds()
+        if(time_float > since):
+            new_data.append({
+                'project_id': d.project_id,
+                'sensor_id': d.sensor_id,
+                'timestamp': time_float,   # use this method to send the timestamp as a float
+                'value': d.value,
+                'data_type': d.data_type
+            })
+            # app.logger.warning('new data found at ' + str(d.timestamp.timestamp()))
+
+    return jsonify(new_data)
+
+# Handle all HTTP requests from Scratch.
+# As of now the only working blocks exist as a ScratchX extension]
+# These blocks allow users to push new data to the database and
+# retrieve a list of all data that meet certain attributes (specified
+# in Scratch)
+@app.route('/scratchx', methods=['POST','GET'])
+def scratchx():
+    if request.method == 'POST':
+    # add some validation/ security screening here
+        project_id = request.form.get('project_id')
+        sensor_id = request.form.get('sensor_id')
+        data_type = request.form.get('data_type')
+        value = request.form.get('value')
+
+        data = DataStory(project_id=int(project_id), data_type=str(data_type), sensor_id=int(sensor_id), value=float(value))
+        db.session.add(data)
+        db.session.commit()
+        app.logger.warning("project_id: %s, data_type: %s, value: %s" \
+            % (str(project_id), str(data_type), str(value)))
+
+        # send_new_value(data);
+        # return "Thanks for posting! Your data has been added to https://wmsinh.org/data-story\n"
+        # return str(data.timestamp.timestamp());
+        return str(data.timestamp.strftime("%Y-%m-%d %H:%M:%S"));
+
+    if request.method == 'GET':
+        project_id = request.args.get('project_id')
+        data_type = request.args.get('data_type')
+        if data_type is None:
+            data_set = DataStory.query.filter_by(project_id=int(project_id)).all()
+        else:
+            data_set = DataStory.query.filter_by(project_id=int(project_id), data_type=str(data_type)).all()
+        values = [];
+        for datum in data_set:
+            values.append(datum.value)
+        return jsonify(values)
+
+
+# socketio uses websocket which doesn't work with apache 
+# but would be a great option if we ever change servers.
+# With this we could remove timed polling and push new data from the server:
+    # @socketio.on('connect')
+    # def test_connect():
+    #     emit('test response', {'data': 'test'})
+
+
+####################################### FORUM ROUTES #######################################
+# These routes were developed for a forum section prototype. Basic functionality is 
+# outlined below, but more work would definitely be needed in order to add this feature
+# to  the site 
 
 @app.route("/forum", methods=['GET','POST'])
 @login_required
@@ -89,178 +308,9 @@ def delete_post(post_id):
     flash('Success! Post "%s" deleted.' % post.body)
     return redirect(url_for('edit_forum'))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = forms.LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-# Add a new user account to the database
-# If the user signs up with a @whitemountainscience.org email address
-# set user.wmsi_userto True so they have full permissions on the site
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = forms.RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        if "whitemountainscience.org" in user.email:
-            user.wmsi_user = True
-            # only for testing, this should be more secure
-        user.data_story = True
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
- 
-### SCHOOL SUBDOMAIN ROUTES ###
-
-# for now always redirect to the gallery index for school subdomains
-# (note: this must route /gallery/ with a _specific trailing
-# slash_ to the index, because the various files in 
-# gallery will redirect to gallery/ with the slash, and
-# flask will return a 404 if we don't add that slash here
-# (see here: http://flask.pocoo.org/docs/0.12/quickstart/#variable-rules))
-@app.route("/", subdomain="<site_subdomain>")
-def school_index(site_subdomain):
-    return redirect("/gallery/") # direct redirect below
-
-# make sure the root page displays the index
-@app.route("/gallery/", subdomain="<site_subdomain>")
-def subdomain_index(site_subdomain):
-    return school_subdomain(site_subdomain, "index.html")
-
-# any requests to gallery items for each subdomain are passed
-# onto the correct school domain subfolder
-# (thumbs up stores files and SETS LINKS assuming that 
-#  files are stored under /gallery/, so we serve files 
-#  statically (from the filesystem) from there)
-@app.route("/gallery/<path:filename>", subdomain="<site_subdomain>")
-def school_subdomain(site_subdomain, filename):
-    if site_subdomain == DEFAULT_SUBDOMAIN: # www
-        return redirect(url_for('index'))
-    else:
-        return serve_school_subdomain(site_subdomain, filename)
-
-
-
-### DATA STORIES ###
-# This section contains routes associated with the Digital Data Stories project./
-# POSTs can be received from a ScratchX project or by using a cURL command:
-# curl -d "project_id=0&data_type=tempC&value=25" -X POST https://wmsinh.org/scratchx
-# The /data-story route and data_story.html page are respondisble for rendering data
-
-
-# Render the data_story.html page. In order to view the page, a user
-# must have the data_story attribute set to true for their account. 
-# While still in development this attribute is set to True for all
-# new users
-@app.route('/data-story')
-@login_required
-def data_story():
-    if current_user.data_story != True:
-        flash("""Sorry, only users with project permissions can see this page. 
-                If you believe your account should be activated with permissions 
-                contact a WMSI administrator to make the change""")
-        return redirect(url_for('forum'))
-    datastory = DataStory.query.all()
-    return render_template('data_story.html', title='Digital Data Stories', datastory=datastory, bgcolor='black')
-
-
-# This route is pinged on a timer by the data_story page to get database updates
-# The code below depends on both the web page and the server being on the same
-# timer. In the past this has run into issues when the webpage and server clocks
-# are set to different timezones
-@app.route('/get_new_data')
-def get_new_data():
-    since = request.args.get('since', 0.0, type=float)
-    all_data = DataStory.query.all()
-    new_data = []
-    time_float = None
-    # app.logger.warning('checking for new data')
-    for d in all_data:
-        if (sys.version_info > (3, 0)):
-            time_float = d.timestamp.timestamp()
-        else:
-            epoch = datetime.datetime.utcfromtimestamp(0)
-            time_float =  (d.timestamp - epoch).total_seconds()
-        if(time_float > since):
-            new_data.append({
-                'project_id': d.project_id,
-                'sensor_id': d.sensor_id,
-                'timestamp': time_float,   # use this method to send the timestamp as a float
-                'value': d.value,
-                'data_type': d.data_type
-            })
-            # app.logger.warning('new data found at' + str(d.timestamp.timestamp()))
-
-    return jsonify(new_data)
-# socketio uses websocket which doesn't work with apache 
-# but would be a great option if we ever change servers.
-# With this we could remove timed polling and push new data from the server:
-    # @socketio.on('connect')
-    # def test_connect():
-    #     emit('test response', {'data': 'test'})
-
-
-# Handle all HTTP requests from Scratch.
-# As of now the only working blocks exist as a ScratchX extension]
-# These blocks allow users to push new data to the database and
-# retrieve a list of all data that meet certain attributes (specified
-# in Scratch)
-@app.route('/scratchx', methods=['POST','GET'])
-def scratchx():
-    if request.method == 'POST':
-    # add some validation/ security screening here
-        project_id = request.form.get('project_id')
-        sensor_id = request.form.get('sensor_id')
-        data_type = request.form.get('data_type')
-        value = request.form.get('value')
-
-        data = DataStory(project_id=int(project_id), data_type=str(data_type), sensor_id=int(sensor_id), value=float(value))
-        db.session.add(data)
-        db.session.commit()
-        app.logger.warning("project_id: %s, data_type: %s, value: %s" \
-            % (str(project_id), str(data_type), str(value)))
-
-        # send_new_value(data);
-        # return "Thanks for posting! Your data has been added to https://wmsinh.org/data-story\n"
-        # return str(data.timestamp.timestamp());
-        return str(data.timestamp.strftime("%Y-%m-%d %H:%M:%S"));
-
-    if request.method == 'GET':
-        project_id = request.args.get('project_id')
-        data_type = request.args.get('data_type')
-        if data_type is None:
-            data_set = DataStory.query.filter_by(project_id=int(project_id)).all()
-        else:
-            data_set = DataStory.query.filter_by(project_id=int(project_id), data_type=str(data_type)).all()
-        values = [];
-        for datum in data_set:
-            values.append(datum.value)
-        return jsonify(values)
-
-
-## IOT ROUTES ###
+####################################### IOT ROUTES #######################################
+# Built in December 2017, these routes support an Internet of Things example by communicating
+# with a Labview VI connected to an EV3 robot
 
 @app.route("/window")
 def window():
--- flask/include/utils.py
+++ flask/include/utils.py
@@ -5,15 +5,23 @@
 import datetime
 import babel
 
+# double check this function for timestamp discrepancies with the web page (JS)
+    # Returns the current UNIX timestamp - based on 
+    # https://stackoverflow.com/a/19801863 and confirmed
+    # at 'man strftime1' on linux
 def get_cur_timestamp():
-    """ Returns the current UNIX timestamp - based on 
-        https://stackoverflow.com/a/19801863 and confirmed
-        at 'man strftime1' on linux"""
     return datetime.datetime.now().strftime("%s")
 
+    # format dates as either full or medium format
+    # Currently this utility is used by the Jinja2 template
+    # in data_story.html. The utility is set to correspond with
+    # the "datetime" filter in flasktest.py
 def format_datetime(value, format='medium'):
     if format == 'full':
         format="EEEE, d. MMMM y 'at' HH:mm:ss"
     elif format == 'medium':
         format="EE dd.MM.y HH:mm:ss"
     return babel.dates.format_datetime(value, format)
+
+# def format_datetime_iso(value)
+#     format=
