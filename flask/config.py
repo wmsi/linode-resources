ccb4878e0e8344bbcb82754c2a0695d79427e232
Marc Bucchieri <wchurch@whitemountainscience.org>
2018-09-05 20:38:24 -0700

    added support for EV3 data import

--- flask/config.py
+++ flask/config.py
@@ -9,6 +9,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVER_NAME = "wmsinh.org" # server hostname required for subdomain support
+    SERVER_NAME = "wmsinh.org" # server hostname required  for subdomain support
 
 #### Important: install flask_moment and test new datetime handling before pushing to git
